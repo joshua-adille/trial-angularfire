75e3cc4f462323bafb079faa8f642d848b52361a
Joshua <joshua.adille14@gmail.com>
2023-10-31 13:46:51 +0800

    added a separate file for the firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { firebaseConfig } from 'src/firebase.config';
 
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBuNcAzI9iRKqwGkfRd2Y3ENX9bMbuanBs',
-  authDomain: 'test-projects-aabaa.firebaseapp.com',
-  projectId: 'test-projects-aabaa',
-  storageBucket: 'test-projects-aabaa.appspot.com',
-  messagingSenderId: '138482524389',
-  appId: '1:138482524389:web:fdd5ac0e6815f87f56b40c',
-};
 
 @NgModule({
   declarations: [AppComponent],
---
+++ src/firebase.config.ts
@@ -0,0 +1,10 @@
+import { FirebaseOptions } from '@angular/fire/app';
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: 'AIzaSyBuNcAzI9iRKqwGkfRd2Y3ENX9bMbuanBs',
+  authDomain: 'test-projects-aabaa.firebaseapp.com',
+  projectId: 'test-projects-aabaa',
+  storageBucket: 'test-projects-aabaa.appspot.com',
+  messagingSenderId: '138482524389',
+  appId: '1:138482524389:web:fdd5ac0e6815f87f56b40c',
+};
