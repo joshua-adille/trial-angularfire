0a5fb85755e2328688a8b0516031eefb1de0578e
Joshua <joshua.adille14@gmail.com>
2023-10-31 10:03:03 +0800

    added angular/fire and imported it in app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBuNcAzI9iRKqwGkfRd2Y3ENX9bMbuanBs',
+  authDomain: 'test-projects-aabaa.firebaseapp.com',
+  projectId: 'test-projects-aabaa',
+  storageBucket: 'test-projects-aabaa.appspot.com',
+  messagingSenderId: '138482524389',
+  appId: '1:138482524389:web:fdd5ac0e6815f87f56b40c',
+};
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
