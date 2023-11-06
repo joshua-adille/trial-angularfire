72f63d1f6adf19a8b307295331a665d036bc10bc
Joshua <joshua.adille14@gmail.com>
2023-11-06 11:10:05 +0800

    added a function in notes.component.ts for future componentizations

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -32,6 +32,8 @@ export class NotesComponent {
   @Output() deleteData = new EventEmitter<string>();
   @Output() categoryData = new EventEmitter<{ data: string; id: string }>();
   @Output() checkboxData = new EventEmitter<{ data: boolean; id: string }>();
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  @Output() updateNotes = new EventEmitter();
 
   pushUpdateData(data: { value: string; id: string }) {
     this.updateData.emit(data);
@@ -46,5 +48,20 @@ export class NotesComponent {
   }
   pushCheckboxData(data: { data: boolean; id: string }) {
     this.checkboxData.emit(data);
+  }
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  updateCategories(choice: string, id: string) {
+    // debugger;
+    // console.log('Clicked!', choice, id);
+    this.service
+      .updateCategory(choice, id)
+      .then((res) => {
+        console.log(res);
+        // this.getData();
+        this.updateNotes.emit();
+      })
+      .catch((error) => {
+        console.log(`There was an error! ${error}`);
+      });
   }
 }
